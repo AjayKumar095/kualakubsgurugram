44db93078fc73ebae8d74df782b6245c39c26a77
Ajay <jaykumar002kori@gmail.com>
2026-06-02 11:38:27 +0530

    commit

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const session    = require('express-session');
 const flash      = require('connect-flash');
 
 const app  = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.use('/css', express.static(path.join(__dirname, 'css')));
 app.use('/js', express.static(path.join(__dirname, 'js')));
